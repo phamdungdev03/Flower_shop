372770beec711f5034b67c7aa310e76895c46256
Nam03 <21122003nam@gmail.com>
2024-10-19 01:20:26 +0700

    xong header

--- index.php
+++ index.php
@@ -1,42 +1,30 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap Example</title>
-    <!-- Bootstrap CSS v5.3.2 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fiorello Header</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
 </head>
 <body>
-    <header class="bg-dark text-white px-3">
-        <nav class="navbar navbar-expand-lg navbar-dark">
-            <div class="container">
-                <div class="d-flex">
-                    <a class="navbar-brand" href="#">Flower Shop</a>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav ms-auto">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Products</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">About</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="d-flex">
-                    <a class="btn btn-outline-secondary me-4" href="#">Sign In</a>
-                    <a class="btn btn-warning">Sign In</a>
-                </div>
-            </div>
+    <header class="header">
+        <div class="logo">
+            <h1><span class="logo-part1">FIOR</span><span class="logo-part2">ELLO</span></h1>
+        </div>
+        <nav class="nav">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Pages</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Elements</a></li>
+            </ul>
         </nav>
+        <div class="cart">
+            <a href="#">Cart (0)</a>
+        </div>
     </header>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
