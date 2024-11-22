8cac61ba46f1c10a1922a01c7a06fe01557f1319
phamvantam <phamvantam1911@gmail.com>
2024-11-22 22:02:11 +0700

    logic review

--- layout/review_product.php
+++ layout/review_product.php
@@ -11,34 +11,42 @@ if (isset($_GET['product_id'])) {
     $imageUrl = './public/uploads/' . htmlspecialchars($product['default_image']);
 }
 ?>
-<div class="review-container">
-    <h2>Đánh Giá Sản Phẩm</h2>
-    <div class="product-info">
-        <img src="<?php echo $imageUrl ?>" alt="Tên Sản Phẩm" class="product-image">
-        <h1 class="product-name"><?php echo $product['product_name'] ?></h1>
-    </div>
-    <div class="review-form">
-        <form id="review-form" action="./actions/handle_review.php" method="post">
-            <input type="hidden" name="product_id" value="<?php echo $product['product_id'] ?>">
-            <div class="form-group">
-                <label for="rating" class="form-label">Lựa chọn:</label>
-                <div class="form-input">
-                    <select id="rating" name="rating" required>
-                        <option value="1">1 sao</option>
-                        <option value="2">2 Sao</option>
-                        <option value="3">3 Sao</option>
-                        <option value="4">4 Sao</option>
-                        <option value="5" selected>5 Sao</option>
-                    </select>
+<?php
+if (isset($_SESSION['user_id'])) {
+?>
+    <div class="review-container">
+        <h2>Đánh Giá Sản Phẩm</h2>
+        <div class="product-info">
+            <img src="<?php echo $imageUrl ?>" alt="Tên Sản Phẩm" class="product-image">
+            <h1 class="product-name"><?php echo $product['product_name'] ?></h1>
+        </div>
+        <div class="review-form">
+            <form id="review-form" action="./actions/handle_review.php" method="post">
+                <input type="hidden" name="product_id" value="<?php echo $product['product_id'] ?>">
+                <div class="form-group">
+                    <label for="rating" class="form-label">Lựa chọn:</label>
+                    <div class="form-input">
+                        <select id="rating" name="rating" required>
+                            <option value="1">1 sao</option>
+                            <option value="2">2 Sao</option>
+                            <option value="3">3 Sao</option>
+                            <option value="4">4 Sao</option>
+                            <option value="5" selected>5 Sao</option>
+                        </select>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <label for="comment" class="form-label">Đánh giá:</label>
-                <div class="form-input">
-                    <textarea id="comment" name="comment" rows="4" required></textarea>
+                <div class="form-group">
+                    <label for="comment" class="form-label">Đánh giá:</label>
+                    <div class="form-input">
+                        <textarea id="comment" name="comment" rows="4" required></textarea>
+                    </div>
                 </div>
-            </div>
-            <button type="submit" class="submit-button">Gửi Đánh Giá</button>
-        </form>
+                <button type="submit" class="submit-button">Gửi Đánh Giá</button>
+            </form>
+        </div>
     </div>
-</div>
+<?php
+} else {
+    echo "<p style='text-align: center;'>Bạn hãy đăng nhập để đánh giá sản phẩm. <a href='login.php'>Đăng nhập</a></p>";
+}
+?>
